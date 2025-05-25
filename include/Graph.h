859769a76821d23e39a0e72d21f8685300b5c89e
nimsolated <armindoosti03@gmail.com>
2025-05-25 10:38:40 -0700

    A couple fixes to Graph

--- include/Graph.h
+++ include/Graph.h
@@ -10,7 +10,6 @@ namespace Graph {
     template <typename T>
     struct Vertex {
         T m_data;
-        std::vector<Vertex*> m_adjacentVertices;
 
         Vertex() : m_data(T()) {}
         explicit Vertex(const T& data) : m_data(data) {}
@@ -23,6 +22,10 @@ namespace Graph {
 
         Edge() : m_from(nullptr), m_to(nullptr), m_weight(0) {}
         explicit Edge(const size_t weight) : m_from(nullptr), m_to(nullptr), m_weight(weight) {}
+        ~Edge() {
+            delete m_from;
+            delete m_to;
+        }
     };
 
     template <typename T>
@@ -30,27 +33,24 @@ namespace Graph {
     public:
         DirectGraph() = default;
         DirectGraph(const DirectGraph& other) {
-            for (auto vertex : other.m_vertices) {
-                m_vertices.push_back(new Vertex<T>(*vertex));
+            for (auto edge : other.m_edges) {
+                m_edges.push_back(new Edge<T>(*edge));
             }
         }
         DirectGraph& operator=(const DirectGraph& other) {
             if (this != &other) {
-                for (auto vertex : other.m_vertices) {
-                    m_vertices.push_back(new Vertex<T>(*vertex));
+                for (auto edge : other.m_edges) {
+                    m_edges.push_back(new Edge<T>(*edge));
                 }
             }
             return *this;
         }
         ~DirectGraph() {
-            for (auto vertex : m_vertices) {
-                delete vertex;
+            for (auto edge : m_edges) {
+                delete edge;
             }
         }
 
-        void addVertex(const T& data) {
-            m_vertices.emplace_back(new Vertex<T>(data));
-        }
         void addEdge(const Vertex<T>* from, const Vertex<T>* to, const size_t weight) {
             auto* newEdge = new Edge<T>(weight);
             newEdge->m_from = from;
@@ -58,7 +58,6 @@ namespace Graph {
             m_edges.emplace_back(newEdge);
         }
     private:
-        std::vector<Vertex<T>*> m_vertices;
         std::vector<Edge<T>*> m_edges;
     };
 }
